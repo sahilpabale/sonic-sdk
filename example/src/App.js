8fd21bb66fb521ee3d4849288488af637152011a
sahilpabale <dev.sahilpabale@gmail.com>
2022-12-15 22:32:27 +0530

    chakra, tsup and example is fixed

--- example/src/App.js
+++ example/src/App.js
@@ -4,8 +4,8 @@ function App() {
   return (
     <div className="App">
       <h1>0xSonic SDK Example</h1>
-      <User /> // Displays the user's address
-      <Connect /> // Connects the user's wallet
+      <User />
+      <Connect />
     </div>
   );
 }
