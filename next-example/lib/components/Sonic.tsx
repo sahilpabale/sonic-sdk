57b0dcbce6245da1c959f5ed27ae939b79b66c28
Anish De <63192115+AnishDe12020@users.noreply.github.com>
2022-12-17 15:37:41 +0530

    comments :pog:

--- next-example/lib/components/Sonic.tsx
+++ next-example/lib/components/Sonic.tsx
@@ -1,7 +1,9 @@
-import { Button, FormControl, FormErrorMessage, FormLabel, Input, Text, VStack } from '@chakra-ui/react';
-import React from 'react';
+import { Button, Flex, FormControl, FormErrorMessage, FormLabel, HStack, Input, Text, VStack } from '@chakra-ui/react';
+import Avatar from '@davatar/react';
+import React, { useEffect } from 'react';
 import { useForm } from 'react-hook-form';
 import orbis from '../orbis.client';
+import { truncateDid } from '../utils/truncate';
 
 interface SonicProps {
   context: string;
@@ -18,6 +20,8 @@ export const Sonic: React.FC<SonicProps> = ({ context }) => {
     formState: { errors }
   } = useForm<PostForm>();
 
+  const [posts, setPosts] = React.useState<IOrbisPost[] | null>(null);
+
   const addComment = (data: PostForm) => {
     console.log('add comment:', data);
 
@@ -26,6 +30,16 @@ export const Sonic: React.FC<SonicProps> = ({ context }) => {
       context: context
     });
   };
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const posts = await orbis.getPosts({ context: context });
+      console.log('posts', posts.data);
+      setPosts(posts.data);
+    };
+
+    fetchPosts();
+  }, [context]);
 
   return (
     <VStack gap={8}>
@@ -40,6 +54,20 @@ export const Sonic: React.FC<SonicProps> = ({ context }) => {
         <Button type="submit" onClick={handleSubmit(addComment)}>
           Add Comment
         </Button>
+
+        {posts &&
+          posts.length > 0 &&
+          posts.map((post) => (
+            <VStack gap={4} rounded="xl" p={4} backgroundColor="brand.tertiary">
+              <HStack gap={2}>
+                <Avatar size={32} address={post.creator_details?.metadata.address as string} />
+                <Text fontSize="sm" fontWeight="semibold">
+                  {truncateDid(post.creator)}
+                </Text>
+              </HStack>
+              <Text>{post.content.body}</Text>
+            </VStack>
+          ))}
       </VStack>
     </VStack>
   );
