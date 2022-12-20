f4495c7e772e633936cee369161002bca2275375
Anish De <63192115+AnishDe12020@users.noreply.github.com>
2022-12-20 20:21:00 +0530

    make replyto modal better

--- next-example/lib/components/ReplyTo.tsx
+++ next-example/lib/components/ReplyTo.tsx
@@ -1,4 +1,4 @@
-import { Drawer, Button, Text, DrawerOverlay, DrawerContent, DrawerCloseButton, DrawerHeader, DrawerBody, DrawerFooter, useDisclosure, Spinner, Box, HStack } from '@chakra-ui/react';
+import { Drawer, Button, Text, DrawerOverlay, DrawerContent, DrawerCloseButton, DrawerHeader, DrawerBody, DrawerFooter, useDisclosure, Spinner, Box, HStack, VStack } from '@chakra-ui/react';
 import React, { useRef, MutableRefObject } from 'react';
 import { FaReply } from 'react-icons/fa';
 import { fetchReplies } from '../utils/posts';
@@ -15,7 +15,7 @@ export const ReplyTo: React.FC<ReplyToProps> = ({ count, master }) => {
   const { isOpen, onOpen, onClose } = useDisclosure();
   const btnRef: MutableRefObject<any> = useRef();
 
-  const { isIdle, isLoading, data, refetch, isFetching } = useQuery('replies', async () => await fetchReplies(master), {
+  const { isLoading, data, refetch } = useQuery(`replies-${master}`, async () => await fetchReplies(master), {
     enabled: false
   });
 
@@ -33,26 +33,20 @@ export const ReplyTo: React.FC<ReplyToProps> = ({ count, master }) => {
       </Button>
       <Drawer isOpen={isOpen} placement="right" onClose={onClose} finalFocusRef={btnRef} size="md">
         <DrawerOverlay />
-        <DrawerContent backgroundColor="gray.800">
+        <DrawerContent backgroundColor="gray.800" overflow="scroll">
           <DrawerCloseButton />
 
           <DrawerHeader>Replies to </DrawerHeader>
-          {isIdle ? (
-            <div>Idle</div>
-          ) : isLoading ? (
+          {isLoading ? (
             <div>loading</div>
-          ) : isFetching ? (
-            <HStack p="8">
-              <Spinner thickness="3px" />
-              <Text>Fetching...</Text>
-            </HStack>
           ) : (
             <div>
-              {' '}
               <DrawerBody>
-                {data?.masterPost && <Post post={data.masterPost} />}
-                <Box bg="brand.quaternary" h="0.8" w="full" mt="8" mb="8" />
-                {data?.replies && data.replies.length > 0 && data.replies.map((post: IOrbisPost) => <Post key={post.stream_id} post={post} />)}
+                <VStack gap={8} as="form" w="full">
+                  {data?.masterPost && <Post post={data.masterPost} />}
+                  <Box bg="brand.quaternary" h="0.8" w="full" />
+                  {data?.replies && data.replies.length > 0 && data.replies.map((post: IOrbisPost) => <Post key={post.stream_id} post={post} />)}
+                </VStack>
               </DrawerBody>
             </div>
           )}
